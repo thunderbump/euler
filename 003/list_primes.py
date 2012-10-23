1d1e1057840d839bbc33c4e25773d38fe92bba98
thunderbump <thunderbump@gmail.com>
2012-10-23 13:05:24 -0700

    list_primes.py now prints all primes below x or prints a list of x primes. should be useful later.

--- 003/list_primes.py
+++ 003/list_primes.py
@@ -4,19 +4,36 @@
 import sys
 
 argc = len(sys.argv)
-usage = """list_primes.py X
-Makes a list of primes less than a given number X"""
+usage = """list_primes.py [below | total] X
+below: Prints all primes less than a given number X
+total: Prints out the first X primes"""
+below = False
+total = False
 
-if argc != 2:
+if argc != 3:
     print(usage)
     sys.exit(1)
-else:
+if sys.argv[1] == "below":
     try:
-        max_val = int(sys.argv[1])
+        max_val = int(sys.argv[2])
+        below = True
     except:
         print(usage)
         print("X must be a number")
         sys.exit(1)
+elif sys.argv[1] == "total":
+    try:
+        max_val = int(sys.argv[2])
+        total = True
+    except:
+        print(usage)
+        print("X must be a number")
+        sys.exit(1)
+else:
+    print(usage)
+    sys.exit(1)
+
+    
 
 def is_prime(number, prime_list):
     for prime in prime_list:
@@ -40,12 +57,29 @@ except:
     primes.append(2)
 reference_file.close()
 
+if below:
+    reference_file = open("primes", 'w')
+    for candidate in range(primes[-1], max_val + 1):
+        if is_prime(candidate, primes):
+            primes.append(candidate)
+    for prime in primes:
+        reference_file.write("%d\n" % prime)
+    reference_file.close()
+    for prime in primes:
+        if prime > max_val:
+            break
+        print(prime),
 
-reference_file = open("primes", 'w')
-for candidate in range(primes[-1], max_val + 1):
-    if is_prime(candidate, primes):
-        primes.append(candidate)
-for prime in primes:
-    reference_file.write("%d\n" % prime)
-reference_file.close()
-
+if total:
+    reference_file = open("primes", 'w')
+    index = primes[-1] + 1
+    while len(primes) < max_val + 1:
+        if is_prime(index, primes):
+            primes.append(index)
+    for prime in primes:
+        reference_file.write("%d\n" % prime)
+    reference_file.close()
+    index = 0
+    while index < max_val:
+        print(primes[index]),
+        index += 1
